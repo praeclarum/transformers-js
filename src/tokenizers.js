a5e62e328cb60c6b21784fa1b3b998ce34b70388
Frank A. Krueger <fak@praeclarum.org>
2022-08-17 16:24:57 -0700

    Run tests

--- src/tokenizers.js
+++ src/tokenizers.js
@@ -1,3 +1,38 @@
 
 
 console.log("Tokenizers");
+
+class Tokenizer {
+    constructor(vocab, normalizer, decoder) {
+        this.vocab = vocab;
+        this.normalizer = normalizer;
+        this.decoder = decoder;
+        this.vocabIndex = {};
+        this.vocab.forEach((token, index) => {
+            this.vocabIndex[String(token[0])] = index;
+        });
+        this.starts = {};
+    }
+    encode(text) {
+        return text.split(" ");
+    }
+    decode(tokens) {
+        return tokens.join(" ");
+    }
+}
+
+async function loadTokenizer(url) {
+    function loadNormalizer(jnormalizer) {
+        switch (jnormalizer.type) {
+            case "Precompiled":
+                return jnormalizer;
+            default:
+                throw new Error("Unknown normalizer type: " + jnormalizer.type);
+        }
+    }
+    const response = await fetch(url);
+    const jtokenizer = await response.json();
+    console.log(jtokenizer);
+    return new Tokenizer(jtokenizer.model.vocab, loadNormalizer(jtokenizer.normalizer), jtokenizer.decoder);
+}
+
