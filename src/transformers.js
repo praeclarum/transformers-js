95275626a02594f496ae0a7310e6e68d80c169fe
Frank A. Krueger <fak@praeclarum.org>
2022-08-19 01:28:23 -0700

    Fix generation loop

--- src/transformers.js
+++ src/transformers.js
@@ -56,19 +56,19 @@ class T5ForConditionalGeneration extends AutoModelForSeq2SeqLM {
         let pastKeyValues = null;
         let outputTokenIds = [initialOutputTokenIds[0]];
         let numOutputTokens = 1;
-        const maxOutputTokens = numOutputTokens + 2;
+        const maxOutputTokens = numOutputTokens + 3;
         while (numOutputTokens < maxOutputTokens) {
             let output = await this.forward(inputTokenIds, outputTokenIds, encoderOutputs, pastKeyValues);
             pastKeyValues = output.pastKeyValues;
             encoderOutputs = output.encoderOutputs;
-            let newTokenId = this.sample(output.logits, numOutputTokens);
+            let newTokenId = this.sample(output.logits);
             outputTokenIds.push(newTokenId);
             numOutputTokens++;
         }
         return outputTokenIds;
     }
 
-    sample(logits, index) {
+    sample(logits) {
         return 42;
     }
 
@@ -78,7 +78,7 @@ class T5ForConditionalGeneration extends AutoModelForSeq2SeqLM {
         const inputIdsTensor = new ort.Tensor("int64", new BigInt64Array(inputIds.map(x => BigInt(x))), [1, inputIds.length]);
         const encoderAttentionMaskTensor = new ort.Tensor("int64", new BigInt64Array(inputIds.length).fill(1n), [1, inputIds.length]);
         if (encoderOutputs === null) {
-            console.log("Encoding...");
+            // console.log("Encoding...");
             const encoderFeeds = {
                 "input_ids": inputIdsTensor,
                 "attention_mask": encoderAttentionMaskTensor,
@@ -86,14 +86,12 @@ class T5ForConditionalGeneration extends AutoModelForSeq2SeqLM {
             const encoderResults = await this.encoderSession.run(encoderFeeds);
             const encoderHiddenStates = encoderResults.hidden_states;
             encoderOutputs = encoderHiddenStates;
-            console.log("Encoding done.", encoderOutputs);
+            // console.log("Encoding done.", encoderOutputs);
         }
 
         const decoderInputIdsTensor = new ort.Tensor("int64", new BigInt64Array(decoderInputIds.map(x => BigInt(x))), [1, decoderInputIds.length]);
-        const decoderAttentionMaskTensor = new ort.Tensor("int64", new BigInt64Array(decoderInputIds.length).fill(1n), [1, decoderInputIds.length]);
+        // const decoderAttentionMaskTensor = new ort.Tensor("int64", new BigInt64Array(decoderInputIds.length).fill(1n), [1, decoderInputIds.length]);
         const decoderFeeds = {
-            // "input_ids": decoderInputIdsTensor,
-            // "encoder_attention_mask": decoderAttentionMaskTensor,
             "input_ids": decoderInputIdsTensor,
             "encoder_attention_mask": encoderAttentionMaskTensor,
             "encoder_hidden_states": encoderOutputs,
@@ -101,43 +99,33 @@ class T5ForConditionalGeneration extends AutoModelForSeq2SeqLM {
         let logits = null;
 
         if (pastKeyValues === null) {
-            console.log("Init Decoding...");
+            // console.log("Init Decoding...");
             const initDecoderResults = await this.initDecoderSession.run(decoderFeeds);
             logits = initDecoderResults.logits;
-            pastKeyValues = this.getPastKeyValues(initDecoderResults);
-            console.log("Init Decoding done.", logits, pastKeyValues);
+            pastKeyValues = this.getPastKeyValues(this.initDecoderSession.outputNames.slice(1), initDecoderResults);
+            // console.log("Init Decoding done.", logits, pastKeyValues);
         }
         else {
-            console.log("Decoding...");
+            // console.log("Decoding...");
             for (const [k, v] of pastKeyValues) {
                 decoderFeeds[k] = v;
             }
             const decoderResults = await this.decoderSession.run(decoderFeeds);
             logits = decoderResults.logits;
-            pastKeyValues = this.getPastKeyValues(decoderResults);
-            console.log("Decoding done.", logits, pastKeyValues);
+            pastKeyValues = this.getPastKeyValues(this.decoderSession.outputNames.slice(1), decoderResults);
+            // console.log("Decoding done.", logits, pastKeyValues);
         }
         return new Seq2SeqLMOutput(logits, pastKeyValues, encoderOutputs);
     }
 
-    getPastKeyValues(decoderResults) {
-        const orig_past_key_values = [];
-        for (const k in decoderResults) {
-            if (decoderResults.hasOwnProperty(k)) {
-                let newK = k;
-                if (newK === "logits") continue;
-                if (newK === "past_key_values" || newK === "output_past_key_values") {
-                    newK = 0;
-                }
-                else {
-                    newK = parseInt(/\d+/.exec(k)[0]);
-                }
-                const v = decoderResults[k];
-                orig_past_key_values.push([newK, v])
-            }
+    getPastKeyValues(pkvNames, decoderResults) {
+        const pkvs = [];
+        for (const i in pkvNames) {
+            const k = pkvNames[i];
+            const v = decoderResults[k];
+            pkvs.push([`pkv_${i}`, v]);
         }
-        const past_key_values = orig_past_key_values.sort((a, b) => a[0] - b[0]).map((x, i) => ["pkv_" + i, x[1]]);
-        return past_key_values;
+        return pkvs;
     }
 }
 
