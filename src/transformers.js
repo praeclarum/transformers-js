182f3ce331472016cb25531ef37c6c3c85aeaca3
Frank A. Krueger <fak@praeclarum.org>
2022-08-19 00:12:24 -0700

    Run the transformer

--- src/transformers.js
+++ src/transformers.js
@@ -15,26 +15,94 @@ class AutoModelForSeq2SeqLM {
     static async fromPretrained(modelId, modelsPath) {
         const modelIdParts = modelId.split('/');
         const modelName = modelIdParts[modelIdParts.length - 1];
-        const initDecoderUrl = `${modelsPath}/${modelName}-init-decoder-quantized.onnx`;
-        const decoderUrl = `${modelsPath}/${modelName}-decoder-quantized.onnx`;
-        const encoderUrl = `${modelsPath}/${modelName}-encoder-quantized.onnx`;
-        return new T5Model(encoderUrl, initDecoderUrl, decoderUrl);
+        const suffix = "-quantized";
+        const initDecoderUrl = `${modelsPath}/${modelName}-init-decoder${suffix}.onnx`;
+        const decoderUrl = `${modelsPath}/${modelName}-decoder${suffix}.onnx`;
+        const encoderUrl = `${modelsPath}/${modelName}-encoder${suffix}.onnx`;
+        return new T5ForConditionalGeneration(encoderUrl, initDecoderUrl, decoderUrl);
     }
     async ensureLoaded() {
-        if (this.encoderSession === null) {
-            this.encoderSession = new onnx.InferenceSession(this.encoderSource);
-            await this.encoderSession.loadModel();
+        if (this.encoderSession === null && this.encoderSource) {
+            console.log('Loading encoder...');
+            this.encoderSession = await ort.InferenceSession.create(this.encoderSource);
+        }
+        if (this.initDecoderSession === null && this.initDecoderSource) {
+            console.log('Loading init decoder...');
+            this.initDecoderSession = await ort.InferenceSession.create(this.initDecoderSource);
+        }
+        if (this.decoderSession === null && this.decoderSource) {
+            console.log('Loading decoder...');
+            this.decoderSession = await ort.InferenceSession.create(this.decoderSource);
+            console.log('Done loading decoder.');
         }
     }
 }
 
 
-class T5Model extends AutoModelForSeq2SeqLM {
+class T5ForConditionalGeneration extends AutoModelForSeq2SeqLM {
     constructor(encoderSource, initDecoderSource, decoderSource) {
         super(encoderSource, initDecoderSource, decoderSource);
     }
     async generate(inputTokens) {
-        this.ensureLoaded();
+        await this.ensureLoaded();
+
+        console.log("Encoding...");
+        const inputIds = new ort.Tensor("int64", new BigInt64Array(inputTokens.map(x => BigInt(x))), [1, inputTokens.length]);
+        const encoderAttentionMask = new ort.Tensor("int64", new BigInt64Array(inputTokens.length).fill(1n), [1, inputTokens.length]);
+        const encoderFeeds = {
+            "input_ids": inputIds,
+            "attention_mask": encoderAttentionMask,
+        }
+        const encoderResults = await this.encoderSession.run(encoderFeeds);
+        const encoderHiddenStates = encoderResults.hidden_states;
+        console.log("Encoding done.", encoderResults);
+
+        console.log("Init Decoding...");
+        const decoderInputIds = new ort.Tensor("int64", new BigInt64Array(inputTokens.map(x => BigInt(x))), [1, inputTokens.length]);
+        const decoderAttentionMask = new ort.Tensor("int64", new BigInt64Array(inputTokens.length).fill(1n), [1, inputTokens.length]);
+        const initDecoderFeeds = {
+            "input_ids": decoderInputIds,
+            "encoder_attention_mask": decoderAttentionMask,
+            "encoder_hidden_states": encoderHiddenStates,
+        };
+        const initDecoderResults = await this.initDecoderSession.run(initDecoderFeeds);
+        const initDecoderPastKeyValues = this.getPastKeyValues(initDecoderResults);
+        console.log("Init Decoding done.", initDecoderResults, initDecoderPastKeyValues);
+
+        console.log("Decoding...");
+        const decoderFeeds = {
+            "input_ids": decoderInputIds,
+            "encoder_attention_mask": decoderAttentionMask,
+            "encoder_hidden_states": encoderHiddenStates,
+        };
+        for (const [k, v] of initDecoderPastKeyValues) {
+            decoderFeeds[k] = v;
+        }
+        const decoderResults = await this.decoderSession.run(decoderFeeds);
+        const logits = decoderResults.logits;
+        const decoderPastKeyValues = this.getPastKeyValues(decoderResults);
+        console.log("Decoding done.", logits, decoderPastKeyValues);
+        return inputTokens;
+    }
+
+    getPastKeyValues(decoderResults) {
+        const orig_past_key_values = [];
+        for (const k in decoderResults) {
+            if (decoderResults.hasOwnProperty(k)) {
+                let newK = k;
+                if (newK === "logits") continue;
+                if (newK === "past_key_values" || newK === "output_past_key_values") {
+                    newK = 0;
+                }
+                else {
+                    newK = parseInt(/\d+/.exec(k)[0]);
+                }
+                const v = decoderResults[k];
+                orig_past_key_values.push([newK, v])
+            }
+        }
+        const past_key_values = orig_past_key_values.sort((a, b) => a[0] - b[0]).map((x, i) => ["pkv_" + i, x[1]]);
+        return past_key_values;
     }
 }
 
